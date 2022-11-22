f6b9664e01d106ab572173819d538a8916211124
sandeepPangare <pangaresandeep96@gmail.com>
2022-11-22 22:10:38 +0530

    Added:Find Maximum Float

--- src/com/bridgelabz/FindTheMaximum.java
+++ src/com/bridgelabz/FindTheMaximum.java
@@ -14,10 +14,22 @@ public class FindTheMaximum {
             max = a3;
         return max;
     }
+    public static Float maxOfFloat(Float a1, Float a2, Float a3){
 
+        Float max = a1;
+
+        if (a2.compareTo(max) > 0)
+            max = a2;
+
+        if (a3.compareTo(max) > 0)
+            max = a3;
+        return max;
+    }
     public static void main(String[] args) {
 
-        System.out.println("**Welcome to Find Maximum Problem Using Generics**");
+        System.out.println("**Welcome to Find Maximum Problem Using Generics**\n");
         System.out.println("The maximum value between the three integer is: " + maxOfInteger(3, 8, 5));
+        System.out.println();
+        System.out.println("The maximum value between the three Float is: " + maxOfFloat(3.1f, 8.1f, 5.1f));
     }
 }
